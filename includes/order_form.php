14bd9ac69ef9dace78fcee9d0cf1fd05f0c780de
NSergey <NSergey@bitbucket.org>
2014-12-15 09:44:50 +0200

    SD-169 fix all messange succsess and error!

--- includes/order_form.php
+++ includes/order_form.php
@@ -6,6 +6,7 @@ header("Pragma: no-cache");
 $_inc = dirname(__FILE__); // includes
 $b_dir = dirname( $_inc ); // folder sites directory
 
+require_once $_inc.'/config.php';
 require_once $_inc.'/lib/class.phpmail.php';
 
 $_mail = new Phpmail;
@@ -28,6 +29,19 @@ function json_function($params, &$smarty) {
 // $_request = $_GET['params'];
 
 $_request = (isset($_POST['params']) and !empty($_POST['params'])) ? $_POST['params']: false;
+
+$smarty->setTemplateDir($config['smarty']['tpl_path']);
+$smarty->setCacheDir($config['smarty']['cache_path']);
+$smarty->setCompileDir($config['smarty']['tpl_path_compile']);
+
+$smarty->registerPlugin("function","year_now","print_current_year");
+$smarty->assign("domain",$config['domain']);
+$smarty->assign("domain_http",$config['domain_http']);
+$smarty->assign("img",$config['path_img']);
+$smarty->assign("css",$config['path_css']);
+$smarty->assign("js",$config['path_js']);
+$smarty ->assign('api_device', $config['api_device']);
+$smarty ->assign('site_id', $config['site_id']);
 
 // register function and modifier
 $smarty->registerPlugin("modifier",'json', 'json_modifier');

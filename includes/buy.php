38f56490b0c57eb862c964c09a3ff2f42eb44da3
Dima R <dima@dizboard.com>
2014-12-18 11:20:07 +0200

    modify

--- includes/buy.php
+++ includes/buy.php
@@ -5,7 +5,7 @@ $_inc = dirname(__FILE__); // includes
 require_once $_inc.'/lib/users/Order.php';
 $order      = new includes\lib\users\Order;
 $user_id    = $order->getUserID();
-$_productID = (!empty($_POST['price']['productID'])) ? (int)$_POST['price']['productID'] : false;
+$_productID = (!empty($_GET['productID'])) ? (int)$_GET['productID'] : false;
 //logged
 if ($_productID) {
     $_url = $order -> createOrder($_productID);

c4cc2acc9aa62110ebfc13ac2302375a6d21235f
NSergey <n.sergey@dizboard.com>
2015-05-14 16:49:27 +0300

    include scroogefrog_tcp!

--- public/index.php
+++ public/index.php
@@ -1,15 +1,13 @@
 <?php
-// require_once './scroogefrog_tcp.php';
-
 date_default_timezone_set("UTC");
 	
+require_once './scroogefrog_tcp.php';
+
 require '../includes/config.php';
 require '../includes/functions.php';
 //require '../fenom/Fenom.php';
 require '../smarty/Smarty.class.php';
 require_once '../includes/lib/Request.php';
-// statictic
-//require_once '../statistic/scroogefrog_udp_tcp.php';
 
 $_obj = new includes\lib\HTTPRequest;
 $_obj -> init();

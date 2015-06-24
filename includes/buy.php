ae3abff4ee4224da66ccbbef60760c93b5f23dd5
NSergey <n.sergey@dizboard.com>
2015-06-24 15:06:54 +0300

    SD-904!

--- includes/buy.php
+++ includes/buy.php
@@ -6,18 +6,12 @@ require_once $_inc.'/lib/users/Order.php';
 $order   = new includes\lib\users\Order;
 $user_id = $order->getUserIdByAuth();
 $_request= (isset($_POST['price']) and !empty($_POST['price'])) ? $_POST['price']: false;
-//$_referer = (isset($_POST['referer']) and !empty($_POST['referer'])) ? $_POST['referer']: false;
-// $_productID = (!empty($_GET['productID'])) ? (int)$_GET['productID'] : false;
-
 
 $_referer = (isset($_COOKIE['orders_referer']) and !empty($_COOKIE['orders_referer'])) ? $_COOKIE['orders_referer'] : "--"; // $_SERVER['HTTP_REFERER']
 $_landing = (isset($_COOKIE['landing']) and !empty($_COOKIE['landing'])) ? $_COOKIE['landing'] : "--";
 
-// $_referer = ($order ->getSession('orders_referer')) ? $order ->getSession('orders_referer'): false;
 $order ->setReferer( $_referer );
 $order ->setLanding( $_landing );
-
- // echo "<pre>"; var_dump( $_REQUEST, $_SESSION, $_COOKIE, $_COOKIE['PHPSESSID'] ); echo "</pre>"; die('stop');
 
 //logged
 if (isset($_request['productID']) and $_productID = (int)$_request['productID']) {
@@ -26,26 +20,14 @@ if (isset($_request['productID']) and $_productID = (int)$_request['productID'])
     if (isset($_GET['_ga'])) {
         $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . '_ga=' . $_GET['_ga'];
     }
-//    if (isset($_GET['_ga'])) {
-//        $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . '_ga=' . $_GET['_ga'];
-//    }
-    
-//    if (isset($_COOKIE['PHPSESSID'])) {
-//        $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . 'PHPSESSID=' . $_COOKIE['PHPSESSID'];
-//    }
-    
     $order -> _redirect($_url);
 }else {
     if (isset($_GET['product'])) {
         try {
             $_url = $order -> createOrderByProduct($user_id, $_GET['product'], isset($_GET['device'])? $_GET['device'] : null);
-//            if (isset($_GET['_ga'])) {
-//                $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . '_ga=' . $_GET['_ga'];
-//            }
-            
-//            if (isset($_COOKIE['PHPSESSID'])) {
-//                $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . 'PHPSESSID=' . $_COOKIE['PHPSESSID'];
-//            }
+            if (isset($_GET['_ga'])) {
+                $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . '_ga=' . $_GET['_ga'];
+            }
             
             $order -> _redirect($_url);
         } catch (CS\Models\Product\ProductNotFoundException $e) {
--- includes/store.php
+++ includes/store.php
@@ -9,26 +9,6 @@ $obj = new includes\lib\users\Order;
 // smarty config
 require_once 'smarty.config.php';
 
-//$smarty = new Smarty;
-//
-//// settings smarty
-//$smarty->compile_check = true;
-//$smarty->debugging = false;
-//$smarty->force_compile = 1;
-//
-//$smarty->setTemplateDir($config['smarty']['tpl_path']);
-//$smarty->setCacheDir($config['smarty']['cache_path']);
-//$smarty->setCompileDir($config['smarty']['tpl_path_compile']);
-//
-//$smarty->registerPlugin("function","year_now","print_current_year");
-//$smarty->assign("domain",$config['domain']);
-//$smarty->assign("domain_http",$config['domain_http']);
-//$smarty->assign("img",$config['path_img']);
-//$smarty->assign("css",$config['path_css']);
-//$smarty->assign("js",$config['path_js']);
-//$smarty ->assign('api_device', $config['api_device']);
-//$smarty ->assign('site_id', $config['site_id']);
-
 /* list order */
 $products = $obj ->getProducts();
 $_sortingProducts = array('basic' => array(), 'premium' => array());
@@ -43,9 +23,6 @@ if(is_array($products)) {
     }
 }
     
-echo "<pre>";
-var_dump( $_sortingProducts );
-echo "</pre>";
 
 /* form_order */
 $_request = (isset($_POST['price']) and !empty($_POST['price'])) ? $_POST['price']: false;

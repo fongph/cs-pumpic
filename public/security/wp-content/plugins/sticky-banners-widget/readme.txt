2e2ab89854752bc956b1e45253f67774f8359df5
NSergey <n.sergey@dizboard.com>
2015-07-03 18:00:06 +0300

    SD-985!

--- public/security/wp-content/plugins/sticky-banners-widget/readme.txt
+++ public/security/wp-content/plugins/sticky-banners-widget/readme.txt
@@ -1,16 +1,16 @@
-=== Sticky Posts Widget ===
+=== Sticky Banners Widget ===
 Contributors: husobj
-Donate link: http://www.benhuson.co.uk/wordpress-plugins/sticky-posts-widget/
-Tags: widget, sticky posts
+Donate link: #
+Tags: widget, sticky banners
 Requires at least: 2.9
 Tested up to: 3.0.4
 Stable tag: 1.0
 
-A simple widget that will display a list of your sticky posts.
+A simple widget that will display a list of your sticky banners.
 
 == Description ==
 
-A simple widget that will display a list of your sticky posts.
+A simple widget that will display a list of your sticky banners.
 
 == Changelog ==
 
@@ -21,5 +21,5 @@ A simple widget that will display a list of your sticky posts.
 == Installation ==
 
 1. Download and unzip the most recent version of this plugin
-2. Upload the sticky-posts-widget folder to /path-to-wordpress/wp-content/plugins/
-3. Login to your WP Admin panel, click Plugins, and activate "Sticky Posts Widget"
+2. Upload the sticky-banners-widget folder to /path-to-wordpress/wp-content/plugins/
+3. Login to your WP Admin panel, click Plugins, and activate "Sticky banners Widget"
--- public/security/wp-content/plugins/sticky-banners-widget/sticky-banners-widget.php
+++ public/security/wp-content/plugins/sticky-banners-widget/sticky-banners-widget.php
@@ -1,33 +1,36 @@
 <?php
 
 /*
-Plugin Name: Sticky Posts Widget
-Plugin URI: http://www.benhuson.co.uk/wordpress-plugins/sticky-posts-widget/
-Description: A simple widget that will display a list of your sticky posts.
-Author: Ben Huson
+Plugin Name: Sticky Banners Widget
+Plugin URI: #
+Description: A simple widget that will display a list of your sticky banners.
+Author: Edii Shadow
 Version: 1.0
-Author URI: http://www.benhuson.co.uk
+Author URI: #
 */
 
-if ( !class_exists( 'WP_Widget_Sticky_Posts' ) ) {
+if ( !class_exists( 'WP_Widget_Sticky_Banners' ) ) {
 	
-	class WP_Widget_Sticky_Posts extends WP_Widget {
+	class WP_Widget_Sticky_Banners extends WP_Widget {
 	
-		function WP_Widget_Sticky_Posts() {
+		function WP_Widget_Sticky_Banners() {
 			
-			$widget_ops = array( 'classname' => 'widget_sticky_posts', 'description' => __( 'Sticky posts on your site' ) );
-			$this->WP_Widget( 'sticky-posts', __( 'Sticky Posts' ), $widget_ops );
-			$this->alt_option_name = 'widget_sticky_posts';
+			$widget_ops = array( 
+                            'classname' => 'widget_sticky_banners', 
+                            'description' => __( 'Sticky banners on your site' ) 
+                        );
+			$this->WP_Widget( 'sticky-banners', __( 'Sticky Banners' ), $widget_ops );
+			$this->alt_option_name = 'widget_sticky_banners';
 	
-			add_action( 'save_post', array( &$this, 'flush_widget_cache' ) );
-			add_action( 'deleted_post', array( &$this, 'flush_widget_cache' ) );
+			add_action( 'save_banner', array( &$this, 'flush_widget_cache' ) );
+			add_action( 'deleted_banner', array( &$this, 'flush_widget_cache' ) );
 			add_action( 'switch_theme', array( &$this, 'flush_widget_cache' ) );
 			
 		}
 	
 		function widget( $args, $instance ) {
 			
-			$cache = wp_cache_get( 'widget_sticky_posts', 'widget' );
+			$cache = wp_cache_get( 'widget_sticky_banners', 'widget' );
 	
 			if ( !is_array( $cache ) )
 				$cache = array();
@@ -40,63 +43,48 @@ if ( !class_exists( 'WP_Widget_Sticky_Posts' ) ) {
 			ob_start();
 			extract( $args );
 	
-			$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Sticky Posts' ) : $instance['title'], $instance, $this->id_base );
-			if ( !$number = (int) $instance['number'] )
-				$number = 10;
-			else if ( $number < 1 )
-				$number = 1;
-			else if ( $number > 15 )
-				$number = 15;
-	
+			$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Sticky Banners' ) : $instance['title'], $instance, $this->id_base );
+			$image_src = apply_filters( 'widget_image_src', empty( $instance['image_src'] ) ? __( 'Sticky Image Src' ) : $instance['image_src'], $instance, $this->id_base );
+                        $link = apply_filters( 'widget_link', empty( $instance['link'] ) ? __( 'Sticky link' ) : $instance['link'], $instance, $this->id_base );
                         
-                        $r = new WP_Query( array( 'showposts' => $number, 
-                                                    'post_status' => 'publish', 
-                                                    // 'post__in' => get_option( 'sticky_posts' ) 
-                                                    'meta_query' => array(
-                                                            array(
-                                                                    'key' => 'popular_sticky_post_border',
-                                                                    'value' => '1',
-                                                            )
-                                                    )
-                                                ) 
-                                            );
-			
+                        echo $before_widget;
+                        echo "<div class='col-lg-12 col-md-12 col-sm-12 padding-null'>";
                         
-			// $r = new WP_Query( array( 'showposts' => $number, 'nopaging' => 0, 'post_status' => 'publish', 'caller_get_posts' => 1, 'post__in' => get_option( 'sticky_posts' ) ) );
+                            echo "<div class='box-right-banners'>";
+
+                                if ( $title ) echo "<label>". $title ."</label>"; // echo $before_title . $title . $after_title;
+
+                                if ( $link ) echo "<a target='_blank' href='". $link ."'>"; 
+                                   if ( $image_src ) echo "<img src='". $image_src ."' />";
+                                if( $link ) echo "</a>";
+
+
+                            echo "</div>";
+                        
+                        echo "</div>";
+                        echo $after_widget;
+                        
+                        
+
+                        // Reset the global $the_post as this query will have stomped on it
+                        wp_reset_postdata();
+	
 			
-			if ( $r->have_posts() ) :
-				
-				echo $before_widget;
-				
-				if ( $title ) echo $before_title . $title . $after_title;
-				echo '<ul>';
-				while ( $r->have_posts() ) : $r->the_post();
-					?>
-					<li><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_title() ? get_the_title() : get_the_ID() ); ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a></li>
-					<?php
-				endwhile;
-				echo '</ul>';
-				echo $after_widget;
-				
-				// Reset the global $the_post as this query will have stomped on it
-				wp_reset_postdata();
-	
-			endif;
-	
 			$cache[$args['widget_id']] = ob_get_flush();
-			wp_cache_set( 'widget_sticky_posts', $cache, 'widget' );
+			wp_cache_set( 'widget_sticky_banners', $cache, 'widget' );
 		}
 	
 		function update( $new_instance, $old_instance ) {
 			
 			$instance = $old_instance;
 			$instance['title'] = strip_tags( $new_instance['title'] );
-			$instance['number'] = (int) $new_instance['number'];
+                        $instance['image_src'] = strip_tags( $new_instance['image_src'] );
+                        $instance['link'] = strip_tags( $new_instance['link'] );
 			$this->flush_widget_cache();
 	
 			$alloptions = wp_cache_get( 'alloptions', 'options' );
-			if ( isset( $alloptions['widget_sticky_posts'] ) )
-				delete_option( 'widget_sticky_posts' );
+			if ( isset( $alloptions['widget_sticky_banners'] ) )
+				delete_option( 'widget_sticky_banners' );
 	
 			return $instance;
 			
@@ -104,34 +92,37 @@ if ( !class_exists( 'WP_Widget_Sticky_Posts' ) ) {
 	
 		function flush_widget_cache() {
 			
-			wp_cache_delete( 'widget_sticky_posts', 'widget' );
+			wp_cache_delete( 'widget_sticky_banners', 'widget' );
 			
 		}
 	
 		function form( $instance ) {
-			
 			$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-			if ( !isset($instance['number']) || !$number = (int) $instance['number'] )
-				$number = 5;
+			$image_src = isset( $instance['image_src'] ) ? esc_attr( $instance['image_src'] ) : '';
+                        $link = isset( $instance['link'] ) ? esc_attr( $instance['link'] ) : '';
 			?>
 			<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
 	
-			<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
-			<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+                        <p><label for="<?php echo $this->get_field_id( 'image_src' ); ?>"><?php _e( 'Image Path:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'image_src' ); ?>" name="<?php echo $this->get_field_name( 'image_src' ); ?>" type="text" value="<?php echo $image_src; ?>" /></p>
+	
+                        <p><label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'link:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo $link; ?>" /></p>
+	
 			<?php
 			
 		}
 		
 	}
 	
-	function init_WP_Widget_Sticky_Posts() {
+	function init_WP_Widget_Sticky_Banners() {
 	
-		register_widget( 'WP_Widget_Sticky_Posts' );
+		register_widget( 'WP_Widget_Sticky_Banners' );
 		
 	}
 	
-	add_action( 'widgets_init', 'init_WP_Widget_Sticky_Posts' );
+	add_action( 'widgets_init', 'init_WP_Widget_Sticky_Banners' );
 
 }
 

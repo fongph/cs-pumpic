2c937ddac9ace6404903a1a014e7da39c9c4c4b3
NSergey <n.sergey@dizboard.com>
2015-07-17 11:33:23 +0300

    fix LastModified in device!

--- includes/compatibility_device.php
+++ includes/compatibility_device.php
@@ -22,6 +22,20 @@ $phoneData = $compatibility->getModel($modelName);
 //        $smarty -> clearCache('compatibility.tpl', 'compatibility_'.date("dmY", strtotime("now")));
 
 if($phoneData){
+    // LastModified
+    $LastModified_unix = strtotime( $phoneData['cdate'] ); // время последнего изменения страницы
+    $LastModified = gmdate("D, d M Y H:i:s \G\M\T", $LastModified_unix);
+    $IfModifiedSince = false;
+    if (isset($_ENV['HTTP_IF_MODIFIED_SINCE']))
+        $IfModifiedSince = strtotime(substr($_ENV['HTTP_IF_MODIFIED_SINCE'], 5));  
+    if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']))
+        $IfModifiedSince = strtotime(substr($_SERVER['HTTP_IF_MODIFIED_SINCE'], 5));
+    if ($IfModifiedSince && $IfModifiedSince >= $LastModified_unix) {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 304 Not Modified');
+        exit;
+    }
+    header('Last-Modified: '. $LastModified);
+    
     $cache_id = 'compatibility_item_'.md5( $phoneData['rowid'].'-'.$phoneData['cdate'].'-'.$phoneData['alies'] );
     $smarty->assign('compatibilityDeviceUri', $_GET['model']);
     $smarty->assign('getSetting', array(

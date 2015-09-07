c21bef1ab7a352d1265b08538f90ecf637e29c6b
NSergey <n.sergey@dizboard.com>
2015-09-07 13:34:36 +0300

    SD-941!

--- includes/buy.php
+++ includes/buy.php
@@ -24,6 +24,20 @@ if (isset($_request['productID']) and $_productID = (int) $_request['productID']
         ));
     }
 
+    $_utm = '';
+    if(isset($_GET['utm_source']) && isset($_GET['utm_medium']) && isset($_GET['utm_campaign'])) {
+    
+        if(!empty($_GET['utm_source']))
+            $_utm .= 'utm_source='.$_GET['utm_source'].'&';
+        if(!empty($_GET['utm_medium']))
+            $_utm .= 'utm_medium='.$_GET['utm_medium'].'&';
+        if(!empty($_GET['utm_campaign']))
+            $_utm .= 'utm_medium='.$_GET['utm_campaign'].'&';
+
+        $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . trim($_utm, '&');
+    
+    }
+    
     $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . 'sessionOption=new&member=new';
     if (isset($_GET['_ga'])) {
         $_url .= (parse_url($_url, PHP_URL_QUERY) ? '&' : '?') . '_ga=' . $_GET['_ga'] ;

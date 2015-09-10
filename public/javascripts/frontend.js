3774ca6416f07be484a424626c91944332a00574
NSergey <n.sergey@dizboard.com>
2015-09-10 14:11:16 +0300

    SD-1265!

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -3432,10 +3432,16 @@ function _LC_OPEN() {
 // LC_API.on_after_load = function()
 LC_API.on_before_load = function()
 {
-    console.log('LC_API:BEFORE'); 
-    if(!LC_API.chat_window_maximized()) {
-        setTimeout( _LC_OPEN, 1000*60*10); // 1000 - 1cек.
-    }
+    
+    var custom_variables = [
+       { name: 'visit', value: '1' }
+    ];
+    LC_API.set_custom_variables(custom_variables);
+    
+//    console.log('LC_API:BEFORE'); 
+//    if(!LC_API.chat_window_maximized()) {
+//        setTimeout( _LC_OPEN, 1000*60*10); // 1000 - 1cек.
+//    }
     
 //	if(LC_API.chat_window_maximized()) {
 //            // LC_API.hide_chat_window();
@@ -3444,17 +3450,16 @@ LC_API.on_before_load = function()
 };
 
 LC_API.on_after_load = function() {
-    console.log('LC_API:AFTER');    
-    var custom_variables = [
-       { name: 'visit', value: '1' }
-    ];
-    LC_API.set_custom_variables(custom_variables);
+//    console.log('LC_API:AFTER');    
+    
         
         
-    //if(LC_API.chat_window_maximized()) {
+    if(LC_API.chat_window_maximized()) {
 //            // LC_API.hide_chat_window();
-          //  LC_API.minimize_chat_window();
-      //  }
+        LC_API.minimize_chat_window();
+    }
+        
+     setTimeout( _LC_OPEN, 1000*60*10); // 1000 - 1cек.   
 }
 
 $(function() {

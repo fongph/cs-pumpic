f55ae37208c2706a50b5e2ed312d9741aab5f022
Roman Lytvynenko <litvinenko95@gmail.com>
2015-09-11 12:14:16 +0300

    SD-1382 widget style fixes, search added

--- public/security/wp-content/themes/cellspy/search.php
+++ public/security/wp-content/themes/cellspy/search.php
@@ -1,40 +1,62 @@
 <?php get_header(); ?>
+	<div class="very_easy">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1">
+					<div class="row">
+						<h1 class="text-center mt10 mb20"><a class="blog-link" href="/security/">Kids' Phone Safety Blog</a></h1>
 
-	<section id="primary" class="site-content">
-		<div id="content" role="main">
+						<div class="archive">
+							<span class="title"><?php printf( __( "Search Results for '<i>%s</i>'", 'cellspy' ), '<span>' . get_search_query() . '</span>' ); ?></span>
+						</div>
 
-		<?php if ( have_posts() ) : ?>
+						<div class="col-lg-8 col-md-8 col-sm-12">
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentytwelve' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header>
+							<?php if ( have_posts() ) : ?>
 
-			<?php twentytwelve_content_nav( 'nav-above' ); ?>
+								<?php
+								global $wp_query;
+								$wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
+								?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
+								<?php while ( have_posts() ) : the_post(); ?>
+								<?php get_template_part( 'content', 'post' ); ?>
+								<?php endwhile; ?>
 
-			<?php twentytwelve_content_nav( 'nav-below' ); ?>
+								<?php
+								if(function_exists('wp_paginate')) {
+									wp_paginate();
+								}
+								?>
 
-		<?php else : ?>
+							<?php else : ?>
 
-			<article id="post-0" class="post no-results not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentytwelve' ); ?></h1>
-				</header>
+								<article id="post-0" class="post no-results not-found">
+									<header class="entry-header">
+										<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentytwelve' ); ?></h1>
+									</header>
 
-				<div class="entry-content">
-					<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentytwelve' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
+									<div class="entry-content">
+										<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentytwelve' ); ?></p>
+										<?php get_search_form(); ?>
+									</div><!-- .entry-content -->
+								</article><!-- #post-0 -->
 
-		<?php endif; ?>
+							<?php endif; ?>
+						</div>
 
-		</div><!-- #content -->
-	</section><!-- #primary -->
+						<div class="col-lg-4 col-md-4 col-sm-12">
+							<?php require_once '../../templates/includes/blog-banner-buy-home.tpl'; ?>
 
+							<div class="feature_list">
+								<div id="secondary" class="widget-area" role="complementary">
+									<?php get_sidebar('main'); ?>
+								</div>
+							</div>
+						</div>
+				</div>
+			</div> <!-- .row -->
+		</div> <!-- .container -->
+	</div> <!-- .very_easy -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>

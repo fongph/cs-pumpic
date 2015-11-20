f3ce784f7a9f1d7f221854fcb63f7ce00ae66aa4
Maxim Bordyuzha <MaStep92@192-168-40-91.bg.local>
2015-11-20 10:03:48 +0200

    SD-1576 - back

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,15 +95,3 @@ $(function() {
        }
 });
 
-
-$('a').each(function() {
-	var a = new RegExp('/' + window.location.host + '/');
-	if(!a.test(this.href)) {
-		$(this).click(function(event) {
-			event.preventDefault();
-			event.stopPropagation();
-			window.open(this.href, '_blank');
-		});
-	}
-})
-

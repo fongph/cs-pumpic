6f530d595b9ffce469bfe74a2a6c1ddabbe07859
Nikolay Verteba <v.nikolay@dizboard.com>
2016-09-20 12:12:15 +0300

    SD-2549 add banner-block

--- public/limitly-parental-app/gulpfile.js
+++ public/limitly-parental-app/gulpfile.js
@@ -1,6 +1,24 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    },
+    port:3001,
+    open: true,
+    notify: false
+  });
+});
+
+gulp.task('html', function(){
+	gulp.src('./index.html')
+			.pipe(browserSync.reload({stream:true}));
+});
 
 gulp.task('one', function () {
   gulp.src('assets/styl/all.styl')
@@ -12,16 +30,22 @@ gulp.task('one', function () {
 
 });
 
+gulp.task('one', function () {
+  gulp.src('assets/styl/*.styl')
+      .pipe(stylus({
+        compress: true
+      }))
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('public/dist/css'))
+			.pipe(browserSync.reload({stream: true}))
+});
 
 // Действия по умолчанию
-gulp.task('default', ['one'], function(){
-gulp.run('one');
-});
+gulp.task('default', ['one', 'browser-sync', 'watch']);
 
 
 // Отслеживаем изменения в файлах
 gulp.task('watch', function(){
-  gulp.watch("./assets/styl/**/*.styl", function(event){
-    gulp.run('one');
-  });
+  gulp.watch("./assets/styl/**/*.styl", ['one']);
+	gulp.watch('./index.html', ['html']);
 });

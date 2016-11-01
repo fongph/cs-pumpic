8ef7e9ece70e1e9593945dd7abecaa235c21cdca
Nikolay Verteba <v.nikolay@dizboard.com>
2016-11-01 12:10:12 +0200

    SD-2707 fix popup position on open

--- assets/js/easyTooltip.js
+++ assets/js/easyTooltip.js
@@ -11,57 +11,102 @@
  *	http://jquery.com
  *
  */
- 
-(function($) {
 
+(function($) {
+	
 	$.fn.easyTooltip = function(options){
-	  
+		
 		// default configuration properties
-		var defaults = {	
+		var defaults = {
 			xOffset: 10,
-			yOffset: 25,
+			yOffset: 15,
 			tooltipId: "easyTooltip",
 			clickRemove: false,
 			content: "",
 			useElement: ""
-		}; 
-			
-		var options = $.extend(defaults, options);  
+		};
+		
+		var options = $.extend(defaults, options);
 		var content;
-				
-		this.each(function() {  				
-			var title = $(this).attr("title");				
-			$(this).hover(function(e){											 							   
+		var deviceWidth = $(window).width();
+		this.each(function() {
+			var title = $(this).attr("title");
+			// $(this).hover(function(e){
+			// 	content = (options.content != "") ? options.content : title;
+			// 	content = (options.useElement != "") ? $("#" + options.useElement).html() : content;
+			// 	$(this).attr("title","");
+			// 	var condition = deviceWidth<(e.pageX + options.xOffset+200);
+			// 	if (content != "" && content != undefined){
+			// 		$("body").append("<div id='"+ options.tooltipId +"'>"+ content +"</div>");
+			// 		if(condition) {
+			// 			$("#" + options.tooltipId)
+			// 					.css("position","absolute")
+			// 					.css("top",((deviceWidth<410) ? e.pageY + options.yOffset : e.pageY - options.yOffset) + "px")
+			// 					.css("left",((deviceWidth<410 ? ((deviceWidth/2)-100) : e.pageX - options.xOffset - 200)) + "px")
+			// 					.css("display","none")
+			// 					.fadeIn("fast")
+			// 		} else {
+			// 		$("#" + options.tooltipId)
+			// 			.css("position","absolute")
+			// 			.css("top",((deviceWidth<410) ? e.pageY + options.yOffset : e.pageY - options.yOffset) + "px")
+			// 			.css("left",((deviceWidth<410) ? ((deviceWidth/2)-100) : e.pageX + options.xOffset) + "px")
+			// 			.css("display","none")
+			// 			.fadeIn("fast")
+			// 		}
+			// 	}
+			// },
+			// function(){
+			// 	$("#" + options.tooltipId).remove();
+			// 	$(this).attr("title",title);
+			// });
+			$(this).on('mouseenter touchstart', function(e){
 				content = (options.content != "") ? options.content : title;
 				content = (options.useElement != "") ? $("#" + options.useElement).html() : content;
-				$(this).attr("title","");									  				
-				if (content != "" && content != undefined){			
-					$("body").append("<div id='"+ options.tooltipId +"'>"+ content +"</div>");		
-					$("#" + options.tooltipId)
-						.css("position","absolute")
-						.css("top",(e.pageY - options.yOffset) + "px")
-						.css("left",(e.pageX + options.xOffset) + "px")						
-						.css("display","none")
-						.fadeIn("fast")
+				$(this).attr("title","");
+				var condition = deviceWidth<(e.pageX + options.xOffset+200);
+				if (content != "" && content != undefined){
+					$("body").append("<div id='"+ options.tooltipId +"'>"+ content +"</div>");
+					if(condition) {
+						$("#" + options.tooltipId)
+								.css("position","absolute")
+								.css("top",((deviceWidth<410) ? e.pageY + options.yOffset : e.pageY - options.yOffset) + "px")
+								.css("left",((deviceWidth<410 ? ((deviceWidth/2)-100) : e.pageX - options.xOffset - 200)) + "px")
+								.css("display","none")
+								.fadeIn("fast")
+					} else {
+						$("#" + options.tooltipId)
+								.css("position","absolute")
+								.css("top",((deviceWidth<410) ? e.pageY + options.yOffset : e.pageY - options.yOffset) + "px")
+								.css("left",((deviceWidth<410) ? ((deviceWidth/2)-100) : e.pageX + options.xOffset) + "px")
+								.css("display","none")
+								.fadeIn("fast")
+					}
 				}
-			},
-			function(){	
+			});
+			$(this).on('mouseleave touchend',function(){
 				$("#" + options.tooltipId).remove();
 				$(this).attr("title",title);
-			});	
+			});
 			$(this).mousemove(function(e){
-				$("#" + options.tooltipId)
-					.css("top",(e.pageY - options.yOffset) + "px")
-					.css("left",(e.pageX + options.xOffset) + "px")					
-			});	
+				var condition = deviceWidth<(e.pageX + options.xOffset+200);
+				if(condition) {
+					$("#" + options.tooltipId)
+							.css("top", ((deviceWidth<410) ? e.pageY + options.yOffset : e.pageY - options.yOffset) + "px")
+							.css("left",((deviceWidth<410 ? ((deviceWidth/2)-100) : e.pageX - options.xOffset - 200)) + "px")
+				} else {
+					$("#" + options.tooltipId)
+							.css("top",((deviceWidth<410) ? e.pageY + options.yOffset : e.pageY - options.yOffset) + "px")
+							.css("left",((deviceWidth<410) ? ((deviceWidth/2)-100) : e.pageX + options.xOffset) + "px")
+				}
+			});
 			if(options.clickRemove){
 				$(this).mousedown(function(e){
 					$("#" + options.tooltipId).remove();
 					$(this).attr("title",title);
-				});				
+				});
 			}
 		});
-	  
+		
 	};
-
+	
 })(jQuery);

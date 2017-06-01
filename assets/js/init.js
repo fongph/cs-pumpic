6e17e9db8b60eef78c67a05aa13dc3d246447347
lakie <k.lena@dizboard.com>
2017-06-01 12:25:59 +0300

    SD-3143 added android exit banner

--- assets/js/init.js
+++ assets/js/init.js
@@ -632,6 +632,14 @@ function getJsonp(_data) {
 
 }
 
+function addEvent(obj, evt, fn) {
+    if (obj.addEventListener) {
+        obj.addEventListener(evt, fn, false);
+    }
+    else if (obj.attachEvent) {
+        obj.attachEvent("on" + evt, fn);
+    }
+}
 
 /*
  *
